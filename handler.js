99d35f560802e825b4d598b844eeedad311c9caf
Andres Esquivel <andres.esquivel@tresastronautas.com>
2022-10-19 11:18:45 -0500

    deploy

--- handler.js
+++ handler.js
@@ -4,7 +4,6 @@ const chromeLambda = require("@sparticuz/chrome-aws-lambda");
 
 module.exports.generate = async (event) => {
   const { body } = event;
-
   const { rawHtml } = JSON.parse(body);
   const htmlContent = rawHtml.toString("utf-8");
   const browser = await chromeLambda.puppeteer.launch({
@@ -12,23 +11,18 @@ module.exports.generate = async (event) => {
     defaultViewport: chromeLambda.defaultViewport,
     executablePath: await chromeLambda.executablePath,
   });
-
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
-
   await page.evaluateHandle("document.fonts.ready");
   const height = await page.evaluate(
     () => document.documentElement.offsetHeight
   );
-
   const pdfBuffer = await page.pdf({
     printBackground: true,
     height: `${height}px`,
     pageRanges: "1",
   });
-
   await browser.close();
-
   return {
     statusCode: 200,
     body: pdfBuffer.toString("base64"),
